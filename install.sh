e35d8fe515a0cfb5deaaa7bed6acc2bb9fd11455
weorau123 <scottpaff1@gmail.com>
2024-11-20 21:01:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ gen64() {
 install_3proxy() {
     echo "Installing 3proxy"
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
+    wget -qO- $URL | tar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
